985bfce88ba6f2351d19f30df77304995b84630a
acanino <anthony.canino1@gmail.com>
2016-02-22 11:58:14 -0500

    Bad test (waterfall_good4 was a duplicate of an old test).

--- tests/waterfall_good4/A.java
+++ tests/waterfall_good4/A.java
@@ -1,21 +1,10 @@
-package waterfall_good4;
+package waterfall_good5;
 
 modes {low <: mid; mid <: high; };
 
-public class A@mode<X <= high, Y <= high, Z <= Y, W <= Z> {
-  public void m1(String@mode<low> s) {
-    s.charAt(0);  // Good!
-  }
-
-  public void m2(String@mode<Y> s) {
-    s.charAt(0);  // Good!
-  }
-
-  public void m3(String@mode<Z> s) {
-    s.charAt(0);  // Good!
-  }
-
-  public void m4(String@mode<W> s) {
-    s.charAt(0);  // Good!
+public class A {
+  public void m1() {
+    String@mode<high> s1 = new String@mode<high>();
+    s1.charAt(0);
   }
 }
